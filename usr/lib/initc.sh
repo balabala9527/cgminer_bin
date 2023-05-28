4a36848ee26b53192a1b54f2b6cb3bb837ed8f96
jiangtao <jiangtao@nano.cn>
2023-05-29 05:35:36 +0800

    1.re-add file

---
+++ usr/lib/initc.sh
@@ -0,0 +1,15 @@
+#/bin/ash
+echo "stop cron"
+/etc/init.d/cron stop
+echo "stop cgminer"
+/etc/init.d/cgminer stop
+setpwm 100
+sleep 1
+downcode etcmem 1 0
+downcode vx 1 0
+sleep 1
+downcode etcmem 1 0
+downcode vx 1 0
+echo "cron status"
+/etc/init.d/cron start
+/etc/init.d/cgminer restart
